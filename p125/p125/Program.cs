a498a3f7b67833c29078e36e32d2648ac4a0f656
ibrahim <ibrahimea@code.edu.az>
2021-10-03 12:20:43 +0400

    Homework

---
+++ p125/p125/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+
+namespace p125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        #region           
+        //alqoritm tek ve cut
+
+        {
+            //int n = 4;
+
+            //if (n == 0) 
+            //{
+            //    Console.WriteLine("zero");
+            //}
+            //else
+            //{
+            //    if (n % 2 == 0) 
+            //    {
+            //        Console.WriteLine("odd");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("even");
+            //    }
+            //    // alqortim 3 ve 7 bolunen 
+            //    n = 21;
+
+            //    if (n == 0)
+
+            //    {
+            //        Console.WriteLine("zero");
+
+            //    }
+            //    else
+            //    {
+            //        if (n % 21 == 0)
+
+            //        {
+            //            Console.WriteLine("divide");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("not divided");
+            #endregion
+
+
+            #region
+            // shkafin kecirilmesi
+            //var a = 4;
+            //var b = 5;
+            //var c = 7;
+            //var d = 12;
+            //var e = 9;
+
+            //if (a<d && b<e)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //   if (c<d && b<e )
+
+            //    {
+            //        Console.WriteLine("yes");
+            //    }
+            //    else
+            //    {
+            //        if (b < d && c < e)
+            //        {
+            //            Console.WriteLine("yes");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("no");
+
+            //        
+            #endregion
+
+
+            #region           
+            ////quvveti olub olmamagi
+            //int n = 8;
+
+            //while (n%2==0)
+            //{
+            //    n /= 2;
+
+            //}
+            //if (n==1) 
+            //{
+            //    Console.WriteLine("quvvetidir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("quvveti deyil");
+            //}
+            #endregion
+
+            #region
+
+            //nece mertebeli olmagi
+
+            //int n = 15;
+            //int count = 1;
+
+            //while (n > 1)
+            //{
+            //    n = n / 10;
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+
+
+            #region
+
+            //// n ve m arasinda nece tek eded 
+
+            //int n = 8;
+            //int m = 20;
+            //int count  = 0;
+
+            //for (int i = n; i <= m; i++)
+            //{
+            //    if (i%2==1)
+            //    {
+            //        count++;
+
+            //    }
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+
+            #region
+            //sade  ve ya murekkeb 
+
+            //int number = 21;
+            //int count = 0;
+
+
+            //for (int i = 2; i*i < number; i++)
+            //{
+            //    if (number%i==0)
+            //    {
+            //        count++;
+
+            //    }
+            //}
+            //if (count == 0)
+            //{
+            //    Console.WriteLine("sade");
+            //}
+            //else 
+
+            //{
+            //    Console.WriteLine("murekkeb");
+            //}
+            #endregion
+
+
+
+            #region
+
+            //// factorial
+
+
+            //int factorial = 1;
+            //int number = 5;
+
+            //for (int i = 1; i < number; i++)
+            //{
+            //    factorial = factorial * i; // 1*1=1 / 1*2=2 / 2*3=6 / 6*4=24 
+
+
+            //}
+            //Console.WriteLine(factorial);
+
+            #endregion
+
+            #region
+            //3 ve 7 bolunme 
+
+            //int num = 50;
+
+            //if (num%21==0)
+            //{
+            //    Console.WriteLine("divided");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not divided");
+            //}
+
+            #endregion
+
+            #region
+            //n ve m arasinda cut ededlerin cemi 
+
+            // int num = 5;
+            // int num2  = 20;
+            //int  sum = 0;
+
+
+            // for (int i = num; i < num2; i++)
+            // {
+            //     if ( i%2!=0) 
+            //     {
+            //         sum += i; 
+
+            //     }
+            // }
+            // Console.WriteLine(sum);
+            #endregion
+
+            #region
+            //movsumler 
+
+            //int num = 16;
+
+            //if (num>12)
+            //{
+            //    Console.WriteLine("false ");
+
+            //}
+
+
+            //if (num == 12 || num < 3)
+            //{
+            //    Console.WriteLine("winter");
+
+            //}
+            //if (num>=3 && num < 6 )
+            //{
+            //    Console.WriteLine("spring");
+            //}
+            //if (num>=6 && num< 9) 
+            //{
+            //    Console.WriteLine("summer");
+            //}
+            //if (num>=9 && num <=11 )
+            //{
+            //    Console.WriteLine("autumn");
+            //}
+            #endregion 
+
+
+        }
+    }
+}
